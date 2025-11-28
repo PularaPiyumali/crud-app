05e8df4e9721f0ff768bb7d1da6aabdd3b5f768a
PularaPiyumali <120791863+PularaPiyumali@users.noreply.github.com>
2025-11-28 16:28:46 +0530

    Added File Upload API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('users', [UserController::class,'getAllUsers']);
@@ -18,4 +19,7 @@ Route::delete('users/{id}',[UserController::class,'delete']);
 Route::get('/index', [UserController::class, 'index']);
 
 Route::get('/categories', [UserController::class, 'showCategories']);
+
+Route::post('/upload-file', [PostController::class, 'upload']);
+
 
